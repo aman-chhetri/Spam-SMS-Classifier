b543f74c12968ef0024e39f796c425855a0c3570
Aman Kshetri <79894697+aman-chhetri@users.noreply.github.com>
2025-03-03 23:51:18 +0530

    Update app.py
    updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
-nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 
 from nltk.stem.porter import PorterStemmer
